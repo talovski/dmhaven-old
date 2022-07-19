95505a4f65b947c4f4bb2f4791e513d8582e429e
Nikita <nika.talovsky@gmail.com>
2022-07-19 08:40:25 +0300

    migrate to vite

---
+++ src/features/wiki/Wiki.tsx
@@ -0,0 +1,12 @@
+import { styled } from '../../styles/stitches.config';
+import { Outlet } from 'react-router-dom';
+
+export const Wiki = () => (
+	<StyledWiki>
+		<Outlet />
+	</StyledWiki>
+);
+
+const StyledWiki = styled('div', {
+	marginLeft: '248px'
+});
